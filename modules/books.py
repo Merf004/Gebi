d5106f3631157b5ef1536e2866a90d43fba61215
MerF <fedimpmerlin@gmail.com>
2024-09-06 00:13:32 +0100

    add_book fuction is ok!!!

--- modules/books.py
+++ modules/books.py
@@ -20,3 +20,50 @@ class Books:
             
     def to_dict(self):
         return {"id":self.id, "title": self.title, "author": self.author, "gender": self.gender, "isbn": self.isbn, "statut": self.statut}
+
+
+
+def add_book():
+    title = ''
+    while len(title) < 3:
+        title = input("Enter book title (minimum 3 characters): ")
+    print("The title entered is valid: ", title)
+    
+    author = ''
+    while len(author) < 3:
+        author = input("Enter book author's name (3 characters minimum): ")
+    print("Author name entered is valid: ", author)
+    gender = ""
+    list_gender = [
+        "Fiction",
+        "Science-fiction",
+        "Fantasy",
+        "Police",
+        "Horror",
+        "Biography",
+        "Essai",
+        "Documentary",
+        "Handbook",
+        "Poetry",
+        "théâtre",
+        "Children",
+        "Young Adult",
+        "BD",
+        "historical",
+        "Utopie",
+        "Dystopie",
+        "Humor"]
+    print(list_gender)
+    while gender  not in list_gender:
+        gender = input("Enter the genre of the book: ")
+    print("The genre entered is valid: ", gender)
+    
+    isbn = ''
+    while not (isbn.isnumeric() and len(isbn) == 13):
+        isbn = input("Enter the book's ISBN (International Standard Book Number). Make sure it contains exactly 13 digits :\n")
+    
+    print("The ISBN entered is valid : ", isbn)    
+    
+    statut = "Available"
+    
+    b = Books(title, author, gender, isbn, statut)
